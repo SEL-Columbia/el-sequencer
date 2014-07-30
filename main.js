953fc22047ffa89f11564de608457562b89eb2be
Chris Tan <csytan@gmail.com>
2014-07-30 17:19:46 -0400

    Basic controls and panning

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 Seq = {};
 
 Seq.init = function() {
-    this.map = L.map('map', {
+    var self = this;
+    self.map = L.map('map', {
             center: [-7.9991, 127.18],
             zoom: 11,
             doubleClickZoom: false
@@ -11,8 +12,19 @@ Seq.init = function() {
     L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
             'subdomains': 'abcd'
         })
-        .addTo(this.map);
-    this.drawGraph();
+        .addTo(self.map);
+    
+    
+    $('.controls_btn').click(function() {
+        var $step = $('.controls_step');
+        var d = parseInt($(this).data('direction'));
+        var index = parseInt($step.text());
+        index += d;
+        $step.text(index);
+        self.drawGraph(index);
+    });
+    
+    self.drawGraph(0);
 };
 
 
@@ -42,6 +54,9 @@ Seq.initData = function(data) {
 Seq.drawGraph = function(index) {
     // Draw grayscale graph
     var self = this;
+    
+    $('g').empty();
+    
     self.nodes.forEach(function(node) {
         self.circle = L.circle([node.y, node.x], 100, {className: 'node'})
             .addTo(self.map);
@@ -51,69 +66,16 @@ Seq.drawGraph = function(index) {
         var from = self.nodes[edge.from];
         var to = self.nodes[edge.to];
         var latlngs = [[from.y, from.x], [to.y, to.x]];
+        
         L.polyline(latlngs, {
             className: i <= index ? 'edge active' : 'edge'
         }).addTo(self.map);
-    });
-};
-
-
-Seq.animate = function(sequence) {
-    var self = Seq;
-    var i = 0;
-    sequence.forEach(function(nodes) {
-        nodes.forEach(function(node) {
-            i++;
-            setTimeout(function() {            
-                var width = window.innerWidth,
-                    height = window.innerHeight;
-                self.draw(node);
-                if (node.type === 'vertice') {
-                    var width = window.innerWidth,
-                        height = window.innerHeight;
-                    self.projection
-                        .translate([width / 2, height / 2]);
-                    
-                    var center = self.projection([node.x, node.y]);
-                    console.log(node, center, self.projection.translate())
-                    self.zoom.translate([width - center[0], height - center[1]])
-                }
-                self.onzoom();
-            }, 200 * i);
-        });
-    });
-};
-
-
-Seq.draw = function(node) {
-    var self = Seq;
-
-    if (node.type === 'vertice') {
-        self.vector
-            .append('circle')
-            .datum(node)
-            .attr('cx', function(d) { return self.projection([d.x, d.y])[0]; })
-            .attr('cy', function(d) { return self.projection([d.x, d.y])[1]; })
-            .attr('r', 2)
-            .attr('class', 'node')
-            .attr('id', function(d) { return d.id; });
-    } else if (node.type === 'edge') {
-        var from = d3.select('#v' + node.from).data()[0];
-        var to = d3.select('#v' + node.to).data()[0];
-
-        if (from && to) {
-            self.vector
-                .append('path')
-                .datum({
-                    type: 'Feature',
-                    geometry: {
-                        type: 'LineString',
-                        coordinates: [[from.x, from.y], [to.x, to.y]]
-                    }
-                })
-                .attr('d', self.path)
-                .attr('class', 'edge');
+        if (i === index) {
+            self.map.panTo(latlngs[0]);
         }
-    }
+    });
 };
+
+
+
 
